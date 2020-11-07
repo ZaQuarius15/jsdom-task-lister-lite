97215b579ed09f32783ab8183b1ad1800511531f
ZaQuarius15 <jzcarlson014@gmail.com>
2020-11-06 21:47:58 -0600

    deliverables hit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const createTaskForm = document.getElementById('create-task-form')
+const taskList = document.getElementById('tasks')
+
+
+createTaskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const toDo = createTaskForm.children[1].value
+  e.target.reset()
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button")
+  li.innerText = toDo;
+  delBtn.innerText = "x"
+  delBtn.className = "del-btn"
+  li.append(delBtn)
+  taskList.appendChild(li);
+  delBtn.addEventListener("click", () => {
+    li.remove()
+  })
+})
